273f15879fb0a81d7f7a3558035c417d50e7341f
Alok Kumar <208224229+alokcoder18@users.noreply.github.com>
2025-08-22 13:53:55 +0530

    basic html with simple math opertions

---
+++ practice/practice.py
@@ -0,0 +1,22 @@
+print("Hello, Duniya")
+name = input("What is your name? ")
+print("Kese ho, " + name)
+  
+
+a =12
+b =3
+print(a/b)
+
+
+a = 30 
+b = 5
+print(a * b)
+
+
+c = 12
+d = 12
+print(c+d)
+
+d = 11
+e = 10
+print(d - e)
