139311085a3a8ad58b532ff91de661b671c84ab2
Alok Kumar <208224229+alokcoder18@users.noreply.github.com>
2025-09-06 22:31:06 +0530

    comments in py

---
+++ class3/class.py
@@ -0,0 +1,18 @@
+# Hey Every one today we are going to learn about comment in python.
+# we can use comment in python by simply using # before the comment.
+# this is a single line comment
+# we can also use cntrol plus / to comment thee line in python.
+
+
+
+"""this is a multi line comment
+we can use triple quotes to make multi line comment in python."""
+#we can also use ctrl plus / to comment multiple lines in python.
+ 
+print("Hello World") # this is a comment after the code.
+# commnet never affect the code. 
+# comments are used to explain the code and make it more readable.
+# comments never appear in the output of the code.
+
+"""this whole page is full of comments but 
+ not a single line of comment will appear in the output of the code"""
