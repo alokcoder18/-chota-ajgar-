0e23625b5976887e9cbc0f11b0b85ef63bbbf468
Alok Kumar <208224229+alokcoder18@users.noreply.github.com>
2025-09-09 12:25:34 +0530

    how to datatype

--- class4/class.py
+++ class4/class.py
@@ -1,7 +1,11 @@
-a = float(input("Enter First value"))
-b = float(input("Enter Second value"))
-if a > b:
-    print("small Value:", b)
-    print("dfjkfdfdsggf")
-else:
-    print("Large value:", b)
+# a = float(input("Enter First value"))
+# b = float(input("Enter Second value"))
+# if a > b:
+#     print("small Value:", b)
+#     print("dfjkfdfdsggf")
+# else:
+#     print("Large value:", b)
+     
+a = 45
+
+print(type(a))
