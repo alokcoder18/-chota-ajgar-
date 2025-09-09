5c17522ffc16c76561f334155236281cd3530f6a
Alok Kumar <208224229+alokcoder18@users.noreply.github.com>
2025-09-09 12:04:19 +0530

    if else statement

---
+++ class4/class.py
@@ -0,0 +1,7 @@
+a = float(input("Enter First value"))
+b = float(input("Enter Second value"))
+if a > b:
+    print("small Value:", b)
+    print("dfjkfdfdsggf")
+else:
+    print("Large value:", b)
